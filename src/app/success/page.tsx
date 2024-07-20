72ce1e49e7be457a91be77662b676873fe998fde
Ntholi Nkhatho <ntholi@live.com>
2024-07-20 05:43:01 +0200

    refactor: Update SuccessPage layout for medium screens
    The code changes in the `page.tsx` file update the layout of the `SuccessPage` component for medium screens. The `md:w-[40vw]` class is added to the `main` element, ensuring that it takes up 40% of the viewport width on medium-sized screens.
    
    This update improves the responsiveness of the page, providing a better user experience on different screen sizes.

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -7,9 +7,14 @@ export default function SuccessPage() {
       <Alert>
         <CircleCheckBig className='size-4' color='green' />
         <AlertTitle>Successful</AlertTitle>
-        <AlertDescription>
+        <AlertDescription className='text-black'>
           Please ensure you complete your registration in person at Limkokwing
-          University on July 22.
+          University. Registration Dates are as follows:
+          <ul className='list-disc pl-5 mt-1 text-green-600'>
+            <li>Diploma Programs: Wednesday 24th July</li>
+            <li>Degree Programs: Thursday 25th July</li>
+            <li className='text-black'>Late Registration Friday 26th July</li>
+          </ul>
         </AlertDescription>
       </Alert>
     </main>
